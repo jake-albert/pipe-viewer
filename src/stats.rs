1ac97ba31b7ddd6d023b160179d69dac0f807a16
jake-albert <jakealbert93@gmail.com>
2022-10-09 18:39:02 +0900

    Refactor Timer to submodule

--- src/stats.rs
+++ src/stats.rs
@@ -1,3 +1,5 @@
+mod timer;
+
 use crossbeam::channel::Receiver;
 use crossterm::{
     cursor, execute,
@@ -5,7 +7,8 @@ use crossterm::{
     terminal::{Clear, ClearType},
 };
 use std::io::{self, Result, Stderr, Write};
-use std::time::{Duration, Instant};
+use std::time::Instant;
+use timer::Timer;
 
 pub fn stats_loop(silent: bool, stats_rx: Receiver<usize>) -> Result<()> {
     let mut total_bytes = 0;
@@ -61,42 +64,5 @@ impl TimeOutput for u64 {
         let (hours, leftover) = (*self / 3600, *self % 3600);
         let (minutes, seconds) = (leftover / 60, leftover % 60);
         format!("{}:{:02}:{:02}", hours, minutes, seconds)
-    }
-}
-
-// Doesn't make sense to print stats on every loop iteration, since we cannot read it.
-// Use this ergonomic timer struct to manage printing on a desired period.
-struct Timer {
-    last_instant: Instant,
-    delta: Duration,
-    period: Duration,
-    countdown: Duration,
-    ready: bool,
-}
-
-impl Timer {
-    fn new() -> Self {
-        let now = Instant::now();
-        Self {
-            last_instant: now,
-            delta: Duration::default(),
-            period: Duration::from_millis(1000),
-            countdown: Duration::default(),
-            ready: true,
-        }
-    }
-
-    fn update(&mut self) {
-        let now = Instant::now();
-        self.delta = now - self.last_instant;
-        self.last_instant = now;
-        self.countdown = self.countdown.checked_sub(self.delta).unwrap_or_else(|| {
-            self.ready = true;
-            self.period
-        })
-    }
-
-    fn reset(&mut self) {
-        self.ready = false;
     }
 }
